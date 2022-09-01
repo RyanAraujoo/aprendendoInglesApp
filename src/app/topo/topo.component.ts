cba1e331eca34475baad37ff6379e6db17cdb5cd
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-09-01 15:50:53 -0300

    view: completada com bootstrap

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  /* o selector pode ser instanciado no appComponent como:
+          1- Classe --> selector: '.app-topo'
+          2- Atributo --> selector: '[app-topo]'
+          3- Elemento  --> selector: 'app-topo'
+  */
+  templateUrl: './topo.component.html',
+  /* o template pode ser inline ou externo
+          templateUrl: './topo.component.html',
+          template: '<h1></h1>',
+  */
+  styleUrls: ['./topo.component.css']
+  /* o style pode ser inline ou externo
+            styleUrls: ['./topo.component.css'],
+            style: ['. exemplo {background-color: black;}']
+  */
+
+})
+export class TopoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { // ciclo de vida angular 
+  }
+
+}
